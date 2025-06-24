38d928e5ff904a9611b6c972774cc8adb2f67aa6
Ja Tar <tarnogorskijan@gmail.com>
2025-06-24 20:23:36 +0200

    No

--- launkey/src/launkey/app.py
+++ launkey/src/launkey/app.py
@@ -17,7 +17,29 @@ class Launkey(toga.App): # pylint: disable=inherit-non-class
         self.main_window.content = main_box
         self.main_window.show()
 
-        #TODO: Add interface
+        buttons_box = toga.Box(direction=COLUMN, style=Pack(margin=5, flex=1))
+        
+        max_rows = 8
+        max_columns = 8
+
+        self.buttons = []
+        for row in range(max_rows):
+            row_box = toga.Box(direction=ROW, style=Pack(margin=5, flex=1))
+            for column in range(max_columns):
+                button = toga.Button(
+                    f"Button {row * max_columns + column + 1}",
+                    on_press=self.button_pressed,
+                    style=Pack(margin=5, height=60, flex=1)
+                )
+                row_box.add(button)
+                self.buttons.append(button)
+            buttons_box.add(row_box)
+
+        main_box.add(buttons_box)
+
+    def button_pressed(self, widget: toga.Button):
+        #keyboard.press_and_release("tab")
+        print(f"Button {widget.text} pressed")
 
 
 def main():
