19bb8f55a8ad51d1974a7ceac6b7ed8c12ff8f5a
Ja Tar <tarnogorskijan@gmail.com>
2025-06-28 22:52:00 +0200

    Poprawki do UI

--- launkey/src/launkey/ui_mainwindow.py
+++ launkey/src/launkey/ui_mainwindow.py
@@ -16,10 +16,10 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QHeaderView,
-    QListView, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QTableWidget, QTableWidgetItem,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QGroupBox, QHBoxLayout,
+    QHeaderView, QLayout, QListView, QMainWindow,
+    QMenu, QMenuBar, QSizePolicy, QStatusBar,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -36,16 +36,50 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.listView = QListView(self.horizontalLayoutWidget)
         self.listView.setObjectName(u"listView")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listView.sizePolicy().hasHeightForWidth())
+        self.listView.setSizePolicy(sizePolicy)
 
         self.horizontalLayout.addWidget(self.listView)
 
-        self.tableWidget = QTableWidget(self.horizontalLayoutWidget)
-        self.tableWidget.setObjectName(u"tableWidget")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setSizeConstraint(QLayout.SizeConstraint.SetNoConstraint)
+        self.tableWidget_2 = QTableWidget(self.horizontalLayoutWidget)
+        if (self.tableWidget_2.columnCount() < 9):
+            self.tableWidget_2.setColumnCount(9)
+        if (self.tableWidget_2.rowCount() < 9):
+            self.tableWidget_2.setRowCount(9)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget_2.setItem(0, 0, __qtablewidgetitem)
+        self.tableWidget_2.setObjectName(u"tableWidget_2")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.tableWidget_2.sizePolicy().hasHeightForWidth())
+        self.tableWidget_2.setSizePolicy(sizePolicy1)
+        self.tableWidget_2.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.tableWidget_2.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.tableWidget_2.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
+        self.tableWidget_2.setRowCount(9)
+        self.tableWidget_2.setColumnCount(9)
+        self.tableWidget_2.horizontalHeader().setVisible(False)
+        self.tableWidget_2.horizontalHeader().setMinimumSectionSize(30)
+        self.tableWidget_2.horizontalHeader().setDefaultSectionSize(30)
+        self.tableWidget_2.verticalHeader().setVisible(False)
+        self.tableWidget_2.verticalHeader().setHighlightSections(True)
 
-        self.horizontalLayout.addWidget(self.tableWidget)
+        self.verticalLayout.addWidget(self.tableWidget_2)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
 
         self.groupBox = QGroupBox(self.horizontalLayoutWidget)
         self.groupBox.setObjectName(u"groupBox")
+        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
 
         self.horizontalLayout.addWidget(self.groupBox)
 
@@ -55,7 +89,8 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 24))
+        self.menubar.setEnabled(False)
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
         self.menuMain = QMenu(self.menubar)
         self.menuMain.setObjectName(u"menuMain")
         MainWindow.setMenuBar(self.menubar)
@@ -69,6 +104,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+
+        __sortingEnabled = self.tableWidget_2.isSortingEnabled()
+        self.tableWidget_2.setSortingEnabled(False)
+        self.tableWidget_2.setSortingEnabled(__sortingEnabled)
+
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Presets", None))
         self.menuMain.setTitle(QCoreApplication.translate("MainWindow", u"Main", None))
     # retranslateUi
